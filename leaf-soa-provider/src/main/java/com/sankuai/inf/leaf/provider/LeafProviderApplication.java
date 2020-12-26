994fb13e8e4dfe11f91c779296f2e3325c902eb5
hgang <hgang.yu@qq.com>
2020-12-26 16:02:10 +0800

    feat-new api project as a rpc provider

---
+++ leaf-soa-provider/src/main/java/com/sankuai/inf/leaf/provider/LeafProviderApplication.java
@@ -0,0 +1,12 @@
+package com.sankuai.inf.leaf.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeafProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeafProviderApplication.class, args);
+	}
+}
